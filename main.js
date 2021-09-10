1d6a0c5df8ee2259f7bc78e3eb62d8a3fb138c58
sricharan187 <74725858+sricharan187@users.noreply.github.com>
2021-09-10 21:31:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ ctx=canvas.getContext("2d");
 var mouseEvent="empty";
 var color = "blue";
 var width_of_line ="2";
-var new_width = screen.width - 70 ;
+var width=screen.width;
+var new_width = width - 70 ;
 var new_height = screen.height - 300 ;
 
 
-if (screen.width<992){
+if (width<992){
     canvas.width=new_width;
     canvas.height=new_height;
     document.body.style.overflow="hidden";
@@ -34,7 +35,7 @@ canvas.addEventListener("touchmove", my_touchmove);
         ctx.lineWidth = width_of_line;
         color = document.getElementById("color").value ;
         width_of_line = document.getElementById("width_of_line").value ;
-        ctx.moveTo( last_pos_touch_x, last_pos_touch_y);
+        ctx.moveTo(last_pos_touch_x, last_pos_touch_y);
         ctx.lineTo(current_pos_touch_x, current_pos_touch_y);
         ctx.stroke();
 
@@ -49,19 +50,19 @@ canvas.addEventListener("touchmove", my_touchmove);
     }
 
 
-    function my_touchstart(e)
+function my_touchstart(e)
     {
         console.log("my_touchstart");
-        color = color;
-        width_of_line = width_of_line;
         last_pos_touch_x=e.touches[0].clientX-canvas.offsetLeft;
         last_pos_touch_y=e.touches[0].clientY-canvas.offsetTop;
     }
 
 function my_mousedown(e) {
-    color = document.getElementById("color").value ;
-    width_of_line = document.getElementById("width_of_line").value ;
-    mouseEvent="mouseDown";
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    if (width>992){
+        mouseEvent="mouseDown";
+    }
 }
 function my_mouseup(e) {
     mouseEvent="mouseUp";
@@ -70,21 +71,19 @@ function my_mouseleave(e) {
     mouseEvent="mouseLeave";
 }
 function my_mousemove(e){
-    current_pos_mouse_x=e.clientX-canvas.offsetLeft;
-    current_pos_mouse_y=e.clientY-canvas.offsetTop;
+    current_pos_mouse_x = e.clientX-canvas.offsetLeft;
+    current_pos_mouse_y = e.clientY-canvas.offsetTop;
 
     if (mouseEvent=="mouseDown"){
-        console.log("my_touchmove");
-         current_pos_mouse_x = e.clientX - canvas.offsetLeft;
-         current_pos_mouse_y = e.clientY - canvas.offsetTop;
+        console.log("my_mousemove");
 
          ctx.beginPath();
          ctx.strokeStyle = color;
          ctx.lineWidth = width_of_line;
          color = document.getElementById("color").value ;
          width_of_line = document.getElementById("width_of_line").value ;
-         ctx.moveTo( last_pos_x ,  last_pos_y);
-         ctx.lineTo(current_pos_mouse_x, current_pos_mouse_y);
+         ctx.moveTo(last_pos_x,last_pos_y);
+         ctx.lineTo(current_pos_mouse_x,current_pos_mouse_y);
          ctx.stroke();
 
         console.log("Last position of x and y coordinates = ");
